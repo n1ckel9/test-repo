067eafac9ca81775a0d1fa48abac466ef682ce26
n1ckel9 <a.a.ksenofontov@gmail.com>
2025-09-11 20:14:37 +0300

    aaa

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
 
+int tick = 0;
+
+void update() {
+    tick++;
+}
+
 int main() {
     std::cout << "Hello World!" << std::endl;
+    for (int i = 0; i < 10; i++) {
+        update();
+    }
     return 0;
 }
