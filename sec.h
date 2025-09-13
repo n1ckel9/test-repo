7a774acbc6d15c2e254b9fe93f3060b4550be100
n1ckel9 <a.a.ksenofontov@gmail.com>
2025-09-13 16:23:21 +0300

    added contest solves

--- sec.h
+++ sec.h
@@ -1,3 +1,26 @@
-#include<iostream>
+#ifndef SEC_H
+#define SEC_H
+
+#include <string>
+
+enum EDayofWeek {
+    MONDAY,
+    SUNDAY
+};
+
+class Car {
+    private:
+        int index;
+    public:
+        Car(int index);
+        int x;  
+        int getIndex();
+};
+
+void swap_a(int &a, int &b);
+
+void swap_pointer(int* a, int* b);
 
 void print(std::string s);
+
+#endif // SEC_H
